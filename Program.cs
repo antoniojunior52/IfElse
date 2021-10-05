fd6e37c6049ed51c625ffd46268969f47a105b92
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2021-10-05 18:48:36 -0300

    ifelse

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using  System ;
+
+namespace  IfElse
+{
+     
+    {  
+          
+          static  void  Main ( string [] args )
+           
+        {
+         
+    
+
+
+             String  mes ;
+           
+
+            Console . WriteLine ( " Digite o dia desejado de acordo com a lista: " );
+            
+            Console . WriteLine ( " | Janeiro | " );
+            
+            Console . WriteLine ( " | Fevereiro | " );
+            
+            Console . WriteLine ( " | Março | " );
+            
+            Console . WriteLine ( " | Abril  | " );
+            
+            Console . WriteLine ( " | Maio    |    " );
+            
+            Console . WriteLine ( " | Junho | " );
+            
+            Console . WriteLine ( " | Julho | " );
+            
+            Console . WriteLine ( " | Agosto | " );
+            
+            Console . WriteLine ( " | Setembro | " );
+            
+            Console . WriteLine ( " | Outubro | " );
+            
+            Console . WriteLine ( " | Novembro | " );
+            
+            Console . WriteLine ( " | Dezembro | " );
+            
+            
+            Console . WriteLine ( " Consulte a tabela e digite o mês desejado: " );
+            mes  =  Console . ReadLine ();
+            
+            if ( mes  ==  " Janeiro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Fevereiro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 28/29 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Março " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Abril " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 30 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Maio " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Junho " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 30 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Julho " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Agosto " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Setembro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 30 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Outubro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Novembro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 30 DIAS! " );
+                
+            }
+            else  if ( mes  ==  " Dezembro " )
+            {
+                Console . WriteLine ( " O MÊS DIGITADO POSSUI 31 DIAS! " );
+                
+            }
+            
+        }
+
+    }
